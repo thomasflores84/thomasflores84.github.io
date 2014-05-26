5dc825f2729211ad1d6472c6f8fcdc7d3c83f55e
thomasflores84 <thomas.flores84@eagles.ewu.edu>
2014-05-26 16:52:25 -0700

    U1

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,20 @@
 // My Scripts
 $(document).foundation();
+
+$('#smButt').click(function() {
+		makePuzzle(100, this);
+	});
+
+$('#medButt').click(function() {
+		makePuzzle(60, this);
+	});
+
+$('#lgButt').click(function() {
+		makePuzzle(40, this);
+	});
+
+function makePuzzle (size, object) {
+	$("#puzzleDiv").html("<div class='puzzle' style='float: left ; margin-right: 50px ;'><img src='img/doh.jpg'>fsadfs</div>");
+	$( "div.puzzle, p" ).puzzle( size );	
+	$(object).css("background", "red");	
+}
